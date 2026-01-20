586180dca8e1a1d8208e7902e777565f94689f72
mindgoner <contact@bartoszbieniek.pl>
2026-01-20 23:07:59 +0100

    Configuration file update

--- config/propagator.php
+++ config/propagator.php
@@ -3,5 +3,27 @@
 declare(strict_types=1);
 
 return [
-    //
+    'table_prefix' => env('PROPAGATOR_TABLE_PREFIX', 'propagator_'),
+
+    'poll_interval' => (int) env('PROPAGATOR_POLL_INTERVAL', 1),
+
+    'remote_url' => env('PROPAGATOR_REMOTE_URL'),
+
+    'basic_auth' => [
+        'key' => env('PROPAGATOR_KEY'),
+        'secret' => env('PROPAGATOR_SECRET'),
+    ],
+
+    // Timestamps are normalized to UTC internally regardless of app timezone.
+    'timezone' => 'UTC',
+
+    'pusher' => [
+        'enabled' => env('PROPAGATOR_PUSHER_APP_ID')
+            && env('PROPAGATOR_PUSHER_APP_KEY')
+            && env('PROPAGATOR_PUSHER_APP_SECRET'),
+        'app_id' => env('PROPAGATOR_PUSHER_APP_ID'),
+        'key' => env('PROPAGATOR_PUSHER_APP_KEY'),
+        'secret' => env('PROPAGATOR_PUSHER_APP_SECRET'),
+        'cluster' => env('PROPAGATOR_PUSHER_APP_CLUSTER'),
+    ],
 ];
