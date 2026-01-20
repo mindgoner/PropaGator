3265fdafd95f6e29eb13dfea72cee7e1dce26114
mindgoner <contact@bartoszbieniek.pl>
2026-01-20 23:21:47 +0100

    Create migration for incoming requests

---
+++ database/migrations/2026_01_20_231548_create_propagator_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        $tableName = config('propagator.table_prefix', 'propagator_') . 'requests';
+
+        Schema::create($tableName, function (Blueprint $table) {
+            $table->id('requestId');
+            $table->string('requestMethod');
+            $table->text('requestUrl');
+            $table->json('requestHeaders');
+            $table->json('requestQueryParams');
+            $table->longText('requestBody');
+            $table->string('requestIp')->nullable();
+            $table->string('requestUserAgent')->nullable();
+            $table->timestampTz('requestReceivedAt')->comment('UTC');
+            $table->timestamps();
+
+            $table->index('requestReceivedAt');
+        });
+    }
+
+    public function down(): void
+    {
+        $tableName = config('propagator.table_prefix', 'propagator_') . 'requests';
+
+        Schema::dropIfExists($tableName);
+    }
+};
